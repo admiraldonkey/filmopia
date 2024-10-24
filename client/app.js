58b7dcd6e080ed81b958af277b7c2dd47f6f7bdf
jamie <level7jamie@gmail.com>
2024-10-24 21:18:06 +0100

    test

--- client/app.js
+++ client/app.js
@@ -16,7 +16,7 @@ const mainLoadingStatus = document.getElementById("reviews-loading");
 
 // Fetch reviews from API
 async function getReviews() {
-  const response = await fetch("https://filmopia.onrender.com/reviews");
+  const response = await fetch("http://filmopia.onrender.com/reviews");
   return await response.json();
 }
 
@@ -75,11 +75,12 @@ async function handleSubmit(event) {
   const formData = new FormData(reviewForm);
   const body = Object.fromEntries(formData);
 
-  const response = await fetch("https://filmopia.onrender.com/reviews", {
+  const response = await fetch("http://filmopia.onrender.com/reviews", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(body),
   });
+  console.log(response);
 }
 
 // Notifies the user that the requested action is in progress and clears when completed
